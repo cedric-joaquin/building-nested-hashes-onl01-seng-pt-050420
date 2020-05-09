a0f41314d8acb584305bc22ad67017bd4749400b
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-09 22:35:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,40 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "Alive"
+      },
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }, 
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+      ]
+    }, 
     :capulet => {
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
+    }
   }
 end
